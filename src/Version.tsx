faed43992a87b5a9f09cfb32b4fc14bd056933a0
cmoulliard <cmoulliard@redhat.com>
2024-03-14 18:46:26 +0100

    Refactor properly the code to use useState and not anymore hasPageBeenRendered useRef
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- src/Version.tsx
+++ src/Version.tsx
@@ -20,85 +20,79 @@ function displayVersions(versions: Version[]) {
 }
 
 function QuarkusVersion() {
-    const [quarkusVersion, setQuarkusVersion] = useState<Version[]>([]);
-    //const [recommendedQuarkusVersion, setRecommendedQuarkusVersion] = useState<string>();
-    //const [_, setDefaultQuarkusVersion] = useState<Version>();
     const [platform, setPlatform] = useState<Platform[]>([]);
-    //const [data, setData] = useState(null);
-    const hasPageBeenRendered = useRef(false);
-    const [query, setQuery] = useState('');
+    const [quarkusVersion, setQuarkusVersion] = useState<Version[]>([]);
+
+    const [recommendedQuarkusVersion, setRecommendedQuarkusVersion] = useState<string>();
+    const [defaultQuarkusVersion, setDefaultQuarkusVersion] = useState<Version>();
+
+    // const hasPageBeenRendered = useRef(false);
+    // const [query, setQuery] = useState('');
 
     const codeQuarkusUrl = 'https://code.quarkus.io';
     const apiUrl = `${codeQuarkusUrl}/api/platforms`
 
-    let defaultQuarkusVersion: Version;
-    // let defaultQuarkusVersion: Version = {id: "3.8", lts: false};
-    let recommendedQuarkusVersion: string;
-
     const fetchData = async () => {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        //setData(newData);
-        setPlatform(data.platforms)
-        setQuarkusVersion(data.platforms[0].streams)
+        const newData = await response.json();
+        setPlatform(newData.platforms)
+        setQuarkusVersion(newData.platforms[0].streams)
+        console.log("Got data")
     }
 
     useEffect(() => {
-        if (hasPageBeenRendered.current) {
-            console.log("hasPageBeenRendered: true");
-        } else {
-            console.log("hasPageBeenRendered: false");
-            fetchData()
-            if (platform.length > 0) {
-                //setQuarkusVersion(platform[0].streams)
-                platform.forEach(p => {
-                    //console.log("Platform : " + p.name);
-                    console.log("Recommended version is: " + p["current-stream-id"]);
-                    recommendedQuarkusVersion = p["current-stream-id"];
-                });
-            }
+        fetchData()
+    }, []);
 
-            if (quarkusVersion.length > 0 && recommendedQuarkusVersion) {
-                displayVersions(quarkusVersion);
-                quarkusVersion.forEach((v) => {
-                    if (v.id === recommendedQuarkusVersion) {
-                        // setDefaultQuarkusVersion(quarkusVersion[idx])
-                        defaultQuarkusVersion = v
-                        console.log("Quarkus version matches the recommended !")
-                    }
-                })
-            }
-            hasPageBeenRendered.current = true;
-        }
-    }, [query]);
+    useEffect(() => {
+        platform.forEach(p => {
+            //console.log("Platform : " + p.name);
+            console.log("Recommended version is: " + p["current-stream-id"]);
+            setRecommendedQuarkusVersion(p["current-stream-id"]);
+        });
+    }, [platform]);
 
-    if (!defaultQuarkusVersion) {
-        return <>Waiting to get default quarkus version ...</>;
+    useEffect(() => {
+        //displayVersions(quarkusVersion);
+        console.log("Try to match the recommended version: " + recommendedQuarkusVersion);
+        quarkusVersion.forEach((v) => {
+            console.log("Quarkus version: " + v.id)
+            if (v.id === recommendedQuarkusVersion) {
+                setDefaultQuarkusVersion(v)
+                // defaultQuarkusVersion = v
+                console.log("Quarkus version matches the recommended !")
+            }
+        })
+    }, [recommendedQuarkusVersion]);
+
+    if (defaultQuarkusVersion) {
+        return (
+            <Autocomplete
+                id="quarkus-versions"
+                isOptionEqualToValue={(option, value) => option.id === value.id}
+                options={quarkusVersion}
+                getOptionLabel={(quarkusVersion) => quarkusVersion.id}
+                // That don't work using a state object: Version[] or Version => defaultValue={defaultQuarkusVersion}
+                // Same remark if we use a variable: let defaultQuarkusVersion: Version;
+                // BUT that works using => {{id: "3.8", lts: false}}
+                //defaultValue={{id: "3.8", lts: false}}
+
+                //defaultValue={{ ...defaultQuarkusVersion }}
+                defaultValue={defaultQuarkusVersion}
+
+                sx={{width: 300, marginTop: 6, marginX: "auto"}}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Select a quarkus version"
+                        size="small"
+                    />
+                )}
+            />)
+    } else {
+        return (<div>Waiting to get the default Quarkus version ...</div>)
     }
 
-    return (
-        <Autocomplete
-            id="quarkus-versions"
-            isOptionEqualToValue={(option, value) => option.id === value.id}
-            options={quarkusVersion}
-            getOptionLabel={(quarkusVersion) => quarkusVersion.id}
-            // That don't work using a state object: Version[] or Version => defaultValue={defaultQuarkusVersion}
-            // Same remark if we use a variable: let defaultQuarkusVersion: Version;
-            // BUT that works using => {{id: "3.8", lts: false}}
-            //defaultValue={{id: "3.8", lts: false}}
-
-            //defaultValue={{ ...defaultQuarkusVersion }}
-            defaultValue={defaultQuarkusVersion}
-
-            sx={{width: 300, marginTop: 6, marginX: "auto"}}
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label="Select a quarkus version"
-                    size="small"
-                />
-            )}
-        />)
 }
 
 export default QuarkusVersion;
